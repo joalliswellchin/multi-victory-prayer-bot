dab9411543ed35ad074695525cd89f5de212b44b
Chin Joel <chinjoel@Chins-MacBook-Pro.local>
2023-05-08 23:21:49 +0800

    fix aws_cdk import issues

--- app/deploy/deploy.py
+++ app/deploy/deploy.py
@@ -1,12 +1,16 @@
-from aws_cdk import (
-    aws_lambda as _lambda,
-    aws_apigateway as apigw,
-    core,
-)
+# from aws_cdk import (
+#     aws_lambda as _lambda,
+#     aws_apigateway as apigw,
+#     core,
+# )
+from constructs import Construct
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_apigateway as apigw
+import aws_cdk as core
 
 class MyStack(core.Stack):
     
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
         
         # create the lambda function
@@ -32,3 +36,7 @@ class MyStack(core.Stack):
             'GET',
             apigw.LambdaIntegration(handler),
         )
+
+app = core.App()
+MyStack(app, 'MyStack')
+app.synth()
